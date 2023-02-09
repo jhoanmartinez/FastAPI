584af5009f7b9b0a0d641e42c5428caec29687b3
jhoanmartinez <jhoao1320@gmail.com>
2023-02-09 18:41:30 -0500

    Setup of cloud database

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import sqlalchemy as sqlalchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 
-DB_URL = "sqlite:///./dbfile.db"
-# Remote DB_URL = "mysql://root:A8v9w2EOnxrYN5Xot5HZ@containers-us-west-177.railway.app:7451/railway"
+#DB_URL = "sqlite:///./dbfile.db"
+#engine = sqlalchemy.create_engine(DB_URL, connect_args={"check_same_thread": False})
 
-engine = sqlalchemy.create_engine(DB_URL, connect_args={"check_same_thread": False})
-
+#DB_URL = "motor://nombre_usuario:password:@urllocalhost:puerto/database_name"
+DB_URL = "postgresql://postgres:DVKAnbD7QwnE0fc00PT5@containers-us-west-73.railway.app:6082/railway"
+engine = sqlalchemy.create_engine(DB_URL)
 SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative.declarative_base()
